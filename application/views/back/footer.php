70d9c86c7792c4e85135dfee6c657171bdaecfbd
bendinuralif <bendipenthol@gmail.com>
2023-06-01 19:12:52 +0700

    first commit

---
+++ application/views/back/footer.php
@@ -0,0 +1,3 @@
+<footer class="main-footer no-print">
+  <strong>Copyright &copy; 2023 <a>REGORA</a>.</strong> All rights reserved.
+</footer>
---
+++ application/views/front/footer.php
@@ -0,0 +1,20 @@
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <hr>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-6">
+        <strong>Copyright &copy; 2023 <a>REGORA</a>.</strong> All rights reserved.
+        </div>
+        <div class="col-xs-6" align="right"><a href="#top">Back to Top</a></div>
+      </div>
+    </div>
+  </footer>
+  </body>
+
+  </html>
