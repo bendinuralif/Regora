45d3ec67596dad1ebf7797e82577ac694ae901e4
bendinuralif <bendipenthol@gmail.com>
2023-07-12 20:37:01 +0700

    foto

--- application/views/front/navbar.php
+++ application/views/front/navbar.php
@@ -8,62 +8,53 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <div class="col-lg-4">
-        <a href="<?php echo base_url() ?>" class="logo">
-          <img src="<?php echo base_url('assets/images/company/REGORA.png'); ?>" alt="JSOFT" width="150px" style="display: block; margin: 10px auto;">
-        </a>
-      </div>
+      <a class="navbar-brand" href="<?php echo base_url() ?>">
+        <img src="<?php echo base_url('assets/images/company/').$company_data->foto.$company_data->foto_type ?>" alt="<?php echo $company_data->company_name ?>" width="100px">
+      </a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="<?php if ($this->uri->segment(1) == "") {
-                      echo "active";
-                    } ?>">
+        <li class="<?php if($this->uri->segment(1) == ""){echo "active";} ?>">
           <a href="<?php echo base_url() ?>">Home </a>
         </li>
-        <li class="<?php if ($this->uri->segment(1) == "event") {
-                      echo "active";
-                    } ?>">
+        <li class="<?php if($this->uri->segment(1) == "event"){echo "active";} ?>">
           <a href="<?php echo base_url('event') ?>"> Event</a>
+        </li>
+        <li class="<?php if($this->uri->segment(1) == "gallery"){echo "active";} ?>">
+          <a href="<?php echo base_url('gallery/album') ?>"> Foto</a>
         </li>
         <li class="dropdown <?php if($this->uri->segment(1) == "about" or $this->uri->segment(1) == "contact"){echo "active";} ?>">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li class="<?php if ($this->uri->segment(1) == "about") {
-                          echo "active";
-                        } ?>">
+            <li class="<?php if($this->uri->segment(1) == "about"){echo "active";} ?>">
               <a href="<?php echo base_url('about') ?>"> Tentang Kami</a>
             </li>
-            <li class="<?php if ($this->uri->segment(1) == "contact") {
-                          echo "active";
-                        } ?>">
+            <li class="<?php if($this->uri->segment(1) == "contact"){echo "active";} ?>">
               <a href="<?php echo base_url('contact') ?>"> Hubungi Kami</a>
             </li>
           </ul>
         </li>
-        <li class="<?php if ($this->uri->segment(1) == "cart" && $this->uri->segment(2) == "") {
-                      echo "active";
-                    } ?>">
+        <li class="<?php if($this->uri->segment(1) == "cart" && $this->uri->segment(2) == ""){echo "active";} ?>">
           <a href="<?php echo base_url('cart') ?>"> Keranjang</a>
         </li>
       </ul>
 
-      <?php if ($this->session->userdata('usertype') != NULL) { ?>
+      <?php if($this->session->userdata('usertype') != NULL){ ?>
         <ul class="nav navbar-nav navbar-right">
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hi, <?php echo $this->session->userdata('username') ?> <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="<?php echo base_url('cart/history') ?>">Riwayat Booking</a></li>
-              <li><a href="<?php echo base_url('auth/edit_profil/') . $this->session->userdata('user_id') ?>">Edit Profil</a></li>
+              <li><a href="<?php echo base_url('auth/edit_profil/').$this->session->userdata('user_id') ?>">Edit Profil</a></li>
               <li><a href="<?php echo base_url('auth/profil') ?>">Profil Saya</a></li>
               <li role="separator" class="divider"></li>
               <li><a href="<?php echo base_url('auth/logout') ?>">Logout</a></li>
             </ul>
           </li>
         </ul>
-      <?php } else { ?>
+      <?php }else{ ?>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="<?php echo base_url('auth/register') ?>">Register</a></li>
           <li><a href="<?php echo base_url('auth/login') ?>">Login</a></li>
